cb3fa55438049e0d6feed3ce511a360339e38a72
Paweł Drewniak <pawel@idiles.com>
2011-11-09 09:59:39 +0000

    facebook stuff init

--- src/uk/ac/gla/route2go/Main.java
+++ src/uk/ac/gla/route2go/Main.java
@@ -3,9 +3,14 @@ package uk.ac.gla.route2go;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 
+//import com.facebook.android.
 import android.os.Bundle;
 
 public class Main extends MapActivity{
+   public static final String APP_ID = "";
+   private Facebook mFacebook;
+   private AsyncFacebookRunner mAsyncRunner;
+
    /** Called when the activity is first created. */
    @Override
    protected boolean isRouteDisplayed() {
@@ -15,6 +20,10 @@ public class Main extends MapActivity{
    public void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.main);
+    
+        mFacebook = new Facebook(APP_ID);
+        mAsyncRunner = new AsyncFacebookRunner(mFacebook);
+
        MapView mapView = (MapView) findViewById(R.id.mapview);
        mapView.setBuiltInZoomControls(true);
    }
