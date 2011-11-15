bd15ebb0d068a2d44d73690925a734e1a6b38bd6
Motiejus Jakštys <desired.mta@gmail.com>
2011-11-15 19:57:23 +0000

    Basic Path displaying in maps

---
+++ src/uk/ac/gla/route2go/pathcalc/DumbPath.java
@@ -0,0 +1,43 @@
+package uk.ac.gla.route2go.pathcalc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
+
+import android.content.Context;
+
+public class DumbPath {
+	Context context;
+	Point start, end;
+	List<Edge> edges;
+	int shortest_time;
+	
+	public List<Edge> getShortestPath() {
+		// always go through home
+		Point home = new Point(55866706, -4260432, "M&P home");
+		
+		edges = new ArrayList<Edge>();
+		edges.add(new Edge(start, home, 600, "bike"));
+		edges.add(new Edge(home, end, 400, "foot"));
+		shortest_time = 1000;
+		
+		return edges;
+	}
+
+	public int getShortestTime() {
+		return shortest_time;
+	}
+
+	public void setStart(String addr, String comment) {
+		start = new Point(context, addr, comment);
+	}
+
+	public void setEnd(String addr, String comment) {
+		end = new Point(context, addr, comment);
+	}
+
+	public DumbPath(Context c) {
+		context = c;
+	}
+}
---
+++ src/uk/ac/gla/route2go/pathcalc/Point.java
@@ -0,0 +1,38 @@
+package uk.ac.gla.route2go.pathcalc;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+
+public class Point {
+	public GeoPoint latlng;
+	public String comment;
+	
+	Point(int lat, int lng, String comment) {
+		latlng = new GeoPoint(lat, lng);
+		this.comment = comment;
+	}
+	
+	Point(Context c, String addr, String comment) {
+		this.comment = comment;
+		Geocoder g = new Geocoder(c);
+		List<Address> address;
+		try {
+			address = g.getFromLocationName(addr, 1);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Could not fetch address");
+		}
+		int lat = (int) (address.get(0).getLatitude() * 1e6);
+		int lng = (int) (address.get(0).getLongitude() * 1e6);
+		// I would normally call the Point constructor here...
+		// Stupid java.
+		
+		latlng = new GeoPoint(lat, lng); 
+	}
+}
