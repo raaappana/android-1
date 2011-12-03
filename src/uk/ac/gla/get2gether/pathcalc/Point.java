1d9f185c4c3f15f5832b5afd6f14821fde7c5ca6
Paweł Drewniak <pawel@idiles.com>
2011-12-03 18:27:16 +0000

    updated opt client; dirty work with geocoding; temporarily giving raw coords

--- src/uk/ac/gla/get2gether/pathcalc/Point.java
+++ src/uk/ac/gla/get2gether/pathcalc/Point.java
@@ -3,11 +3,13 @@ package uk.ac.gla.get2gether.pathcalc;
 import java.io.IOException;
 import java.util.List;
 
+import org.idansof.otp.client.Geocoder;
+import org.idansof.otp.client.GeocoderResult;
+import org.idansof.otp.client.Location;
 import org.mapsforge.android.maps.GeoPoint;
+import org.xmlpull.v1.XmlPullParserException;
 
 import android.content.Context;
-import android.location.Address;
-import android.location.Geocoder;
 
 public class Point {
 	public GeoPoint latlng;
@@ -19,21 +21,43 @@ public class Point {
 	}
 	
 	Point(Context c, String addr, String comment) {
-		Geocoder g = new Geocoder(c);
-		List<Address> address;
+		final String ad_parm = addr;
+		
+		Runnable decoder = new Runnable() {
+			public void run() {
+				List<Location> locs;
+				Geocoder g = new Geocoder("spurga.numeris.lt:8080",
+						"opentripplanner-geocoder/geocode");
+				try {
+					GeocoderResult res = g.geodecode(ad_parm);
+					locs = res.getLocations();
+					int lat = (int) (locs.get(0).getLatitude() * 1e6);
+					int lng = (int) (locs.get(0).getLongitude() * 1e6);
+					latlng = new GeoPoint(lat, lng); 
+					System.out.println(lat+" "+lng);
+				} catch (IOException e) {
+					System.err.println(e.getMessage());
+					e.printStackTrace();
+					throw new RuntimeException("Could not decode address");
+				} catch (XmlPullParserException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
+		
+		Thread decoder_t = new Thread(decoder);
+		decoder_t.start();
 		try {
-			address = g.getFromLocationName(addr, 1);
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
+			decoder_t.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-			throw new RuntimeException("Could not fetch address");
 		}
-		int lat = (int) (address.get(0).getLatitude() * 1e6);
-		int lng = (int) (address.get(0).getLongitude() * 1e6);
+		
 		// I would normally call the Point constructor here...
 		// Stupid java.
 		
 		this.comment = comment;
-		latlng = new GeoPoint(lat, lng); 
 	}
 }
